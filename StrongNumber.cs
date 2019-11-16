c156ac121b94613d32504797beec9d4ea26d8b9e
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-16 18:58:03 +0200

    checks if summary of factoriels is equal to the number

---
+++ StrongNumber.cs
@@ -0,0 +1,30 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Text;
+
+    public class StrongNumber
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int current = 0;
+            int sum = 1;
+            int sameNumber = number;
+            int totalSum = 0;
+
+            while (sameNumber > 0)
+            {
+                current = sameNumber % 10;
+                sameNumber /= 10;
+                sum = 1;
+
+                for (int i = 1; i <= current; i++) { sum *= i;  }
+                totalSum += sum;
+            }
+
+            if (totalSum == number) { Console.WriteLine("yes"); }
+            else { Console.WriteLine("no"); }
+        }
+    }
+}
