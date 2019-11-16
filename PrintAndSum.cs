145ebeb581f586924df43439b3aa78b411a7489e
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-16 18:09:34 +0200

    printing sum of integers

---
+++ PrintAndSum.cs
@@ -0,0 +1,22 @@
+﻿namespace Tech
+{
+    using System;
+    public class PrintAndSum
+    {
+        public static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumer = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = firstNumber; i <= secondNumer; i++)
+            {
+                sum += i;
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
