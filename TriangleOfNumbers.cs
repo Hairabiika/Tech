0c775e558407991d0086b9b466fd91c1f4433c24
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-17 20:20:37 +0200

    each row has the numbers in it

---
+++ TriangleOfNumbers.cs
@@ -0,0 +1,23 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Text;
+
+    public class TriangleOfNumbers
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
