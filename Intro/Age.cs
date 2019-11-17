6ad83f9b4f5e346f6c74832206d1b5e68c8977b5
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-17 21:44:12 +0200

    refers what you are from your age

--- Intro/Age.cs
+++ Intro/Age.cs
@@ -6,7 +6,6 @@
         public static void Main(string[] args)
         {
             int age = int.Parse(Console.ReadLine());
-
             if (age >= 0 && age <= 2) { Console.WriteLine("baby"); }
             if (age >= 3 && age <= 13) { Console.WriteLine("child"); }
             if (age >= 14 && age <= 19) { Console.WriteLine("teenager"); }
