9cef28932a0f11e29703d6aa85272b031e4883d0
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-17 21:42:17 +0200

    Basic syntax

---
+++ Intro/RageExpenses.cs
@@ -0,0 +1,25 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Text;
+
+    public class RageExpenses
+    {
+        public static void Main(string[] args)
+        {
+            int gamesLost = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int brokenHeadsets = gamesLost / 2;
+            int brokenMouses = gamesLost / 3;
+            int brokenKeyboards = gamesLost / 6;
+            int brokenDisplays = brokenKeyboards / 2;
+
+            double expenses = brokenHeadsets * headsetPrice + brokenMouses * mousePrice + brokenKeyboards * keyboardPrice + brokenDisplays * displayPrice;
+            Console.WriteLine($"Rage expenses: {expenses:F2} lv.");
+        }
+    }
+}
