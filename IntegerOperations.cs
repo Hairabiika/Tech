c49cb729b4d027b9498279f2937a50557f1c83b9
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-18 19:36:08 +0200

    ddd first to the second, divide by the third and multiply by the fourth

---
+++ IntegerOperations.cs
@@ -0,0 +1,19 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Text;
+
+    public class IntegerOperations
+    {
+        public static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            int total = ((firstNumber + secondNumber) / thirdNumber) * fourthNumber;
+            Console.WriteLine(total);
+        }
+    }
+}
