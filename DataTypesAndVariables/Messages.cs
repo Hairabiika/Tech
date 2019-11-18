1aa86fe3aa5662428e54657dfbcfae3db04a183d
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-18 09:09:59 +0200

    Add files via upload

---
+++ DataTypesAndVariables/Messages.cs
@@ -0,0 +1,13 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Text;
+
+    public class Messages
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
