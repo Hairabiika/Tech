92909a40509b93e931c49361b239e728511c65e4
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-18 19:54:00 +0200

    letters from given numbers

---
+++ DataTypesAndVariables/PartOfAsciiTable.cs
@@ -0,0 +1,19 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Text;
+
+    public class PartOfAsciiTable
+    {
+        public static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            
+            for (int i = firstNumber; i <= secondNumber; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
