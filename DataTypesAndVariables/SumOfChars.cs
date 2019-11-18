5a002c08eedb158e3f26dc508a8bb22018747849
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-18 19:50:22 +0200

    summary of given letters

---
+++ DataTypesAndVariables/SumOfChars.cs
@@ -0,0 +1,22 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Text;
+
+    public class SumOfChars
+    {
+        public static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < lines; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                sum += (int)letter;
+            }
+
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
