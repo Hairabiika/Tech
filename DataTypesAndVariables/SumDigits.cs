b80c768c75c6233b261e1c8d64393ff0ba3c1529
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-18 19:32:03 +0200

    sammary of all digits to given number

---
+++ DataTypesAndVariables/SumDigits.cs
@@ -0,0 +1,23 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Text;
+
+    public class SumDigits
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sameNumber = number;
+            int sum = 0;
+
+            while (number > 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
