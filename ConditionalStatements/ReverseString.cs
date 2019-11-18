b579c07cb2a3f31a2bcf8b6f8b47bdb190e37185
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-18 08:58:23 +0200

    reverses a given string

---
+++ ConditionalStatements/ReverseString.cs
@@ -0,0 +1,17 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Text;
+
+    public class ReverseString
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.Write(input[i]);
+            }
+        }
+    }
+}
