cbed600cbfdc77c026379f89757c55989b80e67e
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-18 20:03:12 +0200

    all possibilities for triple letters

---
+++ DataTypesAndVariables/TriplesOfLetters.cs
@@ -0,0 +1,27 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Text;
+
+    public class TriplesOfLetters
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    for (int k = 0; k < number; k++)
+                    {
+                        Console.Write((char)(i + 'a'));
+                        Console.Write((char)(j + 'a'));
+                        Console.Write((char)(k + 'a'));
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
