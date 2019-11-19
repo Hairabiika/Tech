e9aa0078206016796d0d6b53f2e50d926d498c86
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-19 15:48:42 +0200

    calculates total amount of spice

---
+++ DataTypesAndVariables/SpiceMustFlow.cs
@@ -0,0 +1,33 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Text;
+
+    public class SpiceMustFlow
+    {
+        public static void Main(string[] args)
+        {
+            int spice = int.Parse(Console.ReadLine());
+            int days = 0;
+            int yield = spice;
+            int total = 0;
+
+            if (yield >= 100)
+            {
+                while (yield >= 100)
+                {
+                    total += yield - 26;
+                    spice -= 26;
+                    days++;
+                    yield -= 10;
+                }
+
+                total -= 26;
+                Console.WriteLine(days);
+                Console.WriteLine(total);
+            }
+
+            else { Console.WriteLine(0); Console.WriteLine(0); }
+        }
+    }
+}
