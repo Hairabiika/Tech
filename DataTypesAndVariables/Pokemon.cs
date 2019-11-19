25b9ec203f20fdd824bc20cce3a1c6415129bbfe
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-19 16:18:21 +0200

    pokemon go console edition

---
+++ DataTypesAndVariables/Pokemon.cs
@@ -0,0 +1,34 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Text;
+
+    public class Pokemon
+    {
+        public static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustion = int.Parse(Console.ReadLine());
+            int counter = 0;
+            int newPower = power;
+            bool less = false;
+
+            while (newPower >= distance)
+            {
+                if (power == 2 * newPower && newPower > exhaustion && exhaustion != 0)
+                {
+                    newPower /= exhaustion;
+                    if (newPower < distance) {less = true; break; }
+                }
+
+                if (less) { break; }
+                newPower -= distance;
+                counter++;
+            }
+
+            Console.WriteLine(newPower);
+            Console.WriteLine(counter);
+        }
+    }
+}
