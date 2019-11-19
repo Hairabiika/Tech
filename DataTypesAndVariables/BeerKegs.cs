c7ab59a6fc3020a94f80b9173736ac15ea21875f
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-19 15:22:07 +0200

    calculates the keg with most volume

---
+++ DataTypesAndVariables/BeerKegs.cs
@@ -0,0 +1,30 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Text;
+
+    public class BeerKegs
+    {
+        public static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            string model = Console.ReadLine(); ;
+            double radius = double.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            double volume = radius * radius * height;
+            string keg = model;
+
+            for (int i = 1; i < lines; i++)
+            {
+                model = Console.ReadLine();
+                radius = double.Parse(Console.ReadLine());
+                height = int.Parse(Console.ReadLine());
+
+                if (radius * radius * height > volume) { keg = model; volume = radius * radius * height; }
+            }
+
+            Console.WriteLine(keg);
+        }
+    }
+}
