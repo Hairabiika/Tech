3664ce3640c91ffb641ebff4fedb3029e32f67cb
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-19 15:11:59 +0200

    checks if there is enough space

---
+++ DataTypesAndVariables/WaterOverflow.cs
@@ -0,0 +1,23 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Text;
+
+    public class WaterOverflow
+    {
+        public static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < lines; i++)
+            {
+                int water = int.Parse(Console.ReadLine());
+                sum += water;
+                if (sum > 255) { Console.WriteLine("Insufficient capacity!");  sum -= water; }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
