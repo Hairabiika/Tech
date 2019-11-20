00a553198be411af80eb2fa38d7f03b320d7eded
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-20 22:40:58 +0200

    shows what message was given

---
+++ DataTypesAndVariables/DecryptingMessage.cs
@@ -0,0 +1,26 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Numerics;
+    using System.Text;
+
+    public class DecryptingMessage
+    {
+        public static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int lines = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+            
+            for (int i = 0; i < lines; i++)
+            {
+
+                char digit = char.Parse(Console.ReadLine());
+                sb.Append((char)(digit + key));
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
