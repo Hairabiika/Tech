2bccb32d924ef0598e4c2ace4487d9d517040068
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-20 22:54:55 +0200

    checks if every open bracket has closen one

---
+++ DataTypesAndVariables/BalancedBrackets.cs
@@ -0,0 +1,29 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Numerics;
+    using System.Text;
+
+    public class BalancedBrackets
+    {
+        public static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for(int i = 0; i < lines; i++)
+            {
+                string input = Console.ReadLine();
+                if (input == "(") { sum++; }
+                if (input == ")") 
+                {
+                    sum--;
+                    if (sum > 0 || sum < 0) { break; }
+                }
+            }
+
+            if (sum == 0) { Console.WriteLine("BALANCED"); }
+            else { Console.WriteLine("UNBALANCED"); }
+        }
+    }
+}
