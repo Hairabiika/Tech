22e25276a700479c3954384c9b7d2a43e90406f2
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-21 12:30:13 +0200

    checks 2 string for common words

---
+++ Arrays/CommonElements.cs
@@ -0,0 +1,32 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Numerics;
+    using System.Text;
+
+    public class Train
+    {
+        public static void Main(string[] args)
+        {
+            string firstArray = Console.ReadLine();
+            string secondArray = Console.ReadLine();
+            string newArray = string.Empty;
+
+            string[] firstTokens = firstArray.Split(' ');
+            string[] secondTokens = secondArray.Split(' ');
+
+            for (int i = 0; i < secondTokens.Length; i++)
+            {
+                for (int j = 0; j < firstTokens.Length; j++)
+                {
+                    if (firstTokens[j] == secondTokens[i])
+                    {
+                        newArray += firstTokens[j] + " ";
+                    }
+                }
+            }
+
+            Console.WriteLine(newArray);
+        }
+    }
+}
