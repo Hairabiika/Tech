bfad074332eb10b699ccd7e14cc89d00ec8b32ff
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-21 12:14:38 +0200

    caclulates total people in the train

---
+++ Arrays/Train.cs
@@ -0,0 +1,27 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Numerics;
+    using System.Text;
+
+    public class Train
+    {
+        public static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            int sum = 0;
+
+            for (int i = 0; i < wagons; i++)
+            {
+                int people = int.Parse(Console.ReadLine());
+                sb.Append(people + " ");
+                sum += people;
+            }
+
+            sb.AppendLine();
+            sb.Append($"{sum}");
+            Console.Write(sb);
+        }
+    }
+}
