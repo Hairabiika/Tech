0472c06baa9ec9cbf179272faca25520ab1c6b92
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-21 17:37:52 +0200

    checks if left integers are bigger than right ones

---
+++ Arrays/TopIntegers.cs
@@ -0,0 +1,40 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Numerics;
+    using System.Text;
+
+    public class TopIntegers
+    {
+        public static void Main(string[] args)
+        {
+            string numbers = Console.ReadLine();
+            string[] tokens = numbers.Split(' ');
+            int counter = 0;
+            
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                counter = 0;
+
+                for (int j = i + 1; j < tokens.Length; j++)
+                {
+                    string firstToken = tokens[i];
+                    int left = int.Parse(firstToken);
+
+                    string secondToken = tokens[j];
+                    int right = int.Parse(secondToken);
+
+                    if (left > right) { counter++; }
+                    else { break; }
+
+                    if (counter == tokens.Length - i - 1)
+                    {
+                        Console.Write(left + " ");
+                    }
+                }
+            }
+
+            Console.WriteLine(tokens[tokens.Length-1]);
+        }
+    }
+}
