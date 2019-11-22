1a1785ebe3cb462549f573f31d464108d0ae5036
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-22 20:28:47 +0200

    checks if there is pair of numbers equal to another number

---
+++ Arrays/MagicSum.cs
@@ -0,0 +1,29 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Numerics;
+    using System.Text;
+
+    public class MagicSum
+    {
+        public static void Main(string[] args)
+        {
+            string array = Console.ReadLine();
+            string[] tokens = array.Split(' ');
+            int sum = int.Parse(Console.ReadLine());
+            string newArray = string.Empty;
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                for (int j = i; j < tokens.Length; j++)
+                {
+                    if (int.Parse(tokens[i]) + int.Parse(tokens[j]) == sum && i != j)
+                    {
+                        Console.WriteLine(tokens[i] + " " + tokens[j]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
