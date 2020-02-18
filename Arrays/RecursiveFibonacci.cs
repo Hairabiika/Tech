142a0632fe8c98edbf91fbdf5138143b0eba1214
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2020-02-18 19:20:00 +0200

    prints fibonacci number of given position

---
+++ Arrays/RecursiveFibonacci.cs
@@ -0,0 +1,22 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Linq;
+
+    public class RecursiveFibonacci
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(recursiveFibonacci(n));
+        }
+
+        public static int recursiveFibonacci (int n)
+        {
+            if (n == 1) { return 1; }
+            if (n == 2) { return 1; }
+            int final = recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+            return final;
+        }
+    }
+}
