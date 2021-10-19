b4db5048bd389ae209bab85efcd40485e847dc77
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2021-10-19 16:41:24 +0300

    prints the characters between two input characters

---
+++ Arrays/CharactersInRange.cs
@@ -0,0 +1,25 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Linq;
+
+    public class CharactersInRange
+    {
+        public static void print (char firstCharacter, char secondCharacter)
+        {
+            if (firstCharacter > secondCharacter) { char temp = firstCharacter; firstCharacter = secondCharacter; secondCharacter = temp; }
+
+            for (char i = firstCharacter ; i < secondCharacter; i++)
+            {
+                if (i == firstCharacter) continue;
+                Console.Write(i + " ");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            char firstCharacter = char.Parse(Console.ReadLine());
+            char secondCharacter = char.Parse(Console.ReadLine());
+            print(firstCharacter, secondCharacter);
+        }
+    }
+}
