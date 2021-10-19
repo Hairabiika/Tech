f285c882164d4a970437ecaa9e4457ee1c9345ee
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2021-10-19 16:24:28 +0300

    Counts the vowels in a word

---
+++ Arrays/VowelsCount.cs
@@ -0,0 +1,26 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Linq;
+
+    public class VowelsCount
+    {
+        public static int vowels (string word)
+        {
+            int count = 0;
+
+            foreach (var letter in word)
+            {
+                if (letter == 'A' || letter == 'a' || letter == 'E' || letter == 'e' || letter == 'U' || letter == 'u'
+                    || letter == 'O' || letter == 'o' || letter == 'I' || letter == 'i') count++;
+            }
+
+            return count;
+        }
+        public static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Console.WriteLine(vowels(word));
+        }
+    }
+}
