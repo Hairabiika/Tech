a64f694da0b42ece1245c5bdefb400f44ffd44d6
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2021-10-19 16:17:02 +0300

    Find the smallest of three numbers with method

---
+++ Arrays/SmallestOfThreeNumbers.cs
@@ -0,0 +1,22 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Linq;
+
+    public class SmallestOfThreeNumbers
+    {
+        public static int smallest (int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber <= secondNumber && firstNumber <= thirdNumber) return firstNumber;
+            if (secondNumber <= firstNumber && secondNumber <= thirdNumber) return secondNumber;
+            else return thirdNumber;
+        }
+        public static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(smallest(firstNumber,secondNumber,thirdNumber));
+        }
+    }
+}
