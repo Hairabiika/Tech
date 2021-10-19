6b38778c196a52e0ee8b53c0068afc26ea6d1a68
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2021-10-19 18:13:54 +0300

    methods for adding and substracting numbers

---
+++ AddAndSubtract.cs
@@ -0,0 +1,29 @@
+﻿namespace Tech
+{
+    using System;
+    using System.Linq;
+
+    public class AddAndSubtract
+    {
+        public static int add (int firstNumber, int secondNumber)
+        {
+            int sum = firstNumber + secondNumber;
+            return sum;
+        }
+
+        public static int substract (int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int sum = add(firstNumber, secondNumber);
+            sum -= thirdNumber;
+            return sum;
+        }
+
+        public static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(substract(firstNumber, secondNumber, thirdNumber));
+        }
+    }
+}
